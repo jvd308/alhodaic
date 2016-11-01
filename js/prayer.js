c818d35e9e71f9e9c3583a19aa85ec9eff9c6420
System Administrator <jamohamm19@gmail.com>
2016-11-01 16:36:31 -0400

    adding alhoda directory to git

---
+++ js/prayer.js
@@ -0,0 +1,8 @@
+var asrvalue = "4:52 pm";
+var maghribvalue = "8:04 pm";
+var ishavalue = "9:25 pm";
+var dateforvalue = "2016-8-11";
+document.getElementById("asrtime").innerHTML = asrvalue;
+document.getElementById("maghribtime").innerHTML = maghribvalue;
+document.getElementById("ishatime").innerHTML = ishavalue;
+document.getElementById("datefortime").innerHTML = dateforvalue;
